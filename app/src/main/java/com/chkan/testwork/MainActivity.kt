8d37472a013000059f7b22c8660082e7104e41e7
git-cdv <chkan.dv@gmail.com>
2021-09-01 03:38:19 +0300

    done

--- app/src/main/java/com/chkan/testwork/MainActivity.kt
+++ app/src/main/java/com/chkan/testwork/MainActivity.kt
@@ -3,14 +3,12 @@ package com.chkan.testwork
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.chkan.testwork.utils.Constans
 
 open class MainActivity : AppCompatActivity() {
-    private var back_pressed : Long = 0
     private lateinit var navController: NavController
     var arg1 : Int = 0
 
@@ -30,24 +28,6 @@ open class MainActivity : AppCompatActivity() {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
-    //переопределяем нажатие кнопки назад - чтобы выходило а не переходило по стеку
-    override fun onBackPressed() {
-        if (back_pressed + 2000 > System.currentTimeMillis()) {
-            moveTaskToBack(true)
-            finish()
-        } else {
-            Toast.makeText(
-                this, "Для выхода нажмите \"назад\" еще раз",
-                Toast.LENGTH_SHORT
-            ).show();
-            back_pressed = System.currentTimeMillis();
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(Constans.TAG, "MainActivity ->onDestroy()")
-    }
    open fun onMenuClicked (id:Int){
        Log.d(Constans.TAG, "MainActivity ->onMenuClicked()")
        arg1 = id
